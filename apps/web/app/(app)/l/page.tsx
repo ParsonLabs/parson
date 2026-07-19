071d264b78b2a7863f58dd8f1ddf69d79d47033c
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 21:45:51 +0100

    Del(l/page.tsx): Remove Legacy L Page

--- apps/web/app/(app)/l/page.tsx
+++
@@ -1,9 +0,0 @@
-import LibraryButtons from "@/components/Layout/LibraryButtons";
-
-export default function LibraryPage() {
-  return (
-    <div className="min-h-screen text-center">
-      <LibraryButtons />
-    </div>
-  );
-}

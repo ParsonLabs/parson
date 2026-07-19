c146bc416d2a126a1c97ea7d7d96433f61294466
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 22:07:15 +0100

    Del(formatReleaseDate.ts): Remove Legacy Format Release Date

--- apps/web/lib/Formatting/formatReleaseDate.ts
+++
@@ -1,3 +0,0 @@
-export default function formatReleaseDate(releaseDateString: string): string {
-  return new Date(releaseDateString).toLocaleString('default', { month: 'long', year: 'numeric' });
-}

77c2c5a506612fc9a7aca139e737c8940d9b456b
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 20:31:07 +0100

    Feat(playlist/page): Use Playlist Details with Route Metadata

--- apps/web/app/(app)/playlist/page.tsx
+++ apps/web/app/(app)/playlist/page.tsx
@@ -1,10 +1,17 @@
-import PlaylistComponent from "./PlaylistComponent";
+import type { Metadata } from "next";
+import PlaylistDetails from "./playlist-details";
+import RouteLoading from "@/components/app/route-loading";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Playlist",
+  description: "Play and manage a playlist.",
+};
 
 export default function PlaylistPage() {
   return (
-    <Suspense>
-      <PlaylistComponent />
+    <Suspense fallback={<RouteLoading />}>
+      <PlaylistDetails />
     </Suspense>
-  )
+  );
 }

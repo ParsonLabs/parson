b9c62b3a67f09bf079c45b76aadfdf145b528271
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 22:06:00 +0100

    Del(isDesktopApp.ts): Remove Legacy Is Desktop App

--- apps/web/lib/Conditional/isDesktopApp.ts
+++
@@ -1,7 +0,0 @@
-import isElectron from "./isElectron";
-import isPWA from "./isPWA";
-import isTauri from "./isTauri";
-
-export default function isDesktopApp(): boolean {
-  return isElectron() || isPWA() || isTauri();
-}

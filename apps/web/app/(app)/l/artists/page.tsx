4a7007ed66719e2751f14c801a3f80dc81d62bed
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 21:45:36 +0100

    Del(artists/page.tsx): Remove Legacy Artists Page

--- apps/web/app/(app)/l/artists/page.tsx
+++
@@ -1,9 +0,0 @@
-import LibraryButtons from "@/components/Layout/LibraryButtons";
-
-export default function ArtistsPage() {
-  return (
-    <div className="min-h-screen text-center">
-      <LibraryButtons initialSelectedField="Artists" />
-    </div>
-  );
-}

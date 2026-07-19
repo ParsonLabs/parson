7bc901d23c3be9374cf3a90687b65491b8c4936f
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 21:45:55 +0100

    Del(playlists/page.tsx): Remove Legacy Playlists Page

--- apps/web/app/(app)/l/playlists/page.tsx
+++
@@ -1,9 +0,0 @@
-import LibraryButtons from "@/components/Layout/LibraryButtons";
-
-export default function PlaylistsPage() {
-  return (
-    <div className="min-h-screen text-center">
-      <LibraryButtons initialSelectedField="Playlists" />
-    </div>
-  );
-}

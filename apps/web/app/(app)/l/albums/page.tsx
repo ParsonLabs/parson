420c86a068c0aa655851f664fd96dc51b7999ddb
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 21:45:20 +0100

    Del(albums/page.tsx): Remove Legacy Albums Page

--- apps/web/app/(app)/l/albums/page.tsx
+++
@@ -1,9 +0,0 @@
-import LibraryButtons from "@/components/Layout/LibraryButtons";
-
-export default function AlbumsPage() {
-  return (
-    <div className="min-h-screen text-center">
-      <LibraryButtons initialSelectedField="Albums" />
-    </div>
-  );
-}

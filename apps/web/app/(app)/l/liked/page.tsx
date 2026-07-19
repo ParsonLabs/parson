7bf85dfb75524e489a3180a5add827ede0e70eb8
WillKirkmanM <WillKirkmanMoller@gmail.com>
2026-07-19 21:45:42 +0100

    Del(liked/page.tsx): Remove Legacy Liked Page

--- apps/web/app/(app)/l/liked/page.tsx
+++
@@ -1,9 +0,0 @@
-import LibraryButtons from "@/components/Layout/LibraryButtons";
-
-export default function LikedPage() {
-  return (
-    <div className="min-h-screen text-center">
-      <LibraryButtons initialSelectedField="Songs" />
-    </div>
-  );
-}
